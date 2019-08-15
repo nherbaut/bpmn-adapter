29a08b198c74aae34a21486e9ae7e19b18aeacdc
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-08-15 11:49:04 +0200

    reading of rootElements

--- src/main/java/fr/pantheonsorbonne/cri/App.java
+++ src/main/java/fr/pantheonsorbonne/cri/App.java
@@ -1,17 +1,42 @@
 package fr.pantheonsorbonne.cri;
 
-import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
-/**
- * Hello world!
- *
- */
 public class App {
-	public static void main(String[] args) {
+
+	public static <T extends TRootElement> List<T> getRootElements(TDefinitions defs, Class<T> klass) {
+		List<T> res  = (List<T>) defs.getRootElement().stream()
+				.map(e->e.getValue())
+				.filter(obj-> obj.getClass().isAssignableFrom(klass))
+				.map(obj-> klass.cast(obj))
+				.collect(Collectors.toList());
+		
+		return res;
+	}
 	
+	
+	public static TDefinitions readBPMNFromEmbdedModel(String modelName) throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+		StreamSource ss = new StreamSource(App.class.getClassLoader().getResourceAsStream("choreography.xml"));
+		JAXBElement<TDefinitions> o = jaxbUnmarshaller.unmarshal(ss, TDefinitions.class);
+		return o.getValue();
+	}
+
+	public static void main(String[] args) throws JAXBException {
+
+		List<TMessage> defs = getRootElements(readBPMNFromEmbdedModel("choreograph.xml"), TMessage.class);
+
+		defs.stream().map(def -> def.getName()).forEach(System.out::println);
+
 	}
 }
--- src/test/java/fr/pantheonsorbonne/cri/AppTest.java
+++ src/test/java/fr/pantheonsorbonne/cri/AppTest.java
@@ -1,13 +1,8 @@
 package fr.pantheonsorbonne.cri;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.transform.stream.StreamSource;
 
 import org.junit.Test;
 
@@ -16,15 +11,9 @@ public class AppTest {
 	@Test
 	public void shouldAnswerWithTrue() throws JAXBException {
 
-		JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
-		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-		
-		
-		StreamSource ss = new StreamSource(App.class.getClassLoader().getResourceAsStream("choreography.xml"));
-		JAXBElement<TDefinitions> o =  jaxbUnmarshaller.unmarshal(ss,TDefinitions.class);
+		assertNotNull("failed to unmarshall domain object",App.readBPMNFromEmbdedModel("choreography.xml"));
 
-		assertTrue("we don't unmarshall domain object",
-				o.getValue().getClass().getName().equals("fr.pantheonsorbonne.cri.TDefinitions"));
+		
 
 	}
 }
