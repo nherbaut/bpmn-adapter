5e6ec32a5379e694ab7adbd5a42876395f030b0a
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-08-15 11:05:48 +0200

    reading BPMN2.0 models

--- src/main/java/fr/pantheonsorbonne/cri/App.java
+++ src/main/java/fr/pantheonsorbonne/cri/App.java
@@ -1,13 +1,17 @@
 package fr.pantheonsorbonne.cri;
 
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+	
+	}
 }
--- src/test/java/fr/pantheonsorbonne/cri/AppTest.java
+++ src/test/java/fr/pantheonsorbonne/cri/AppTest.java
@@ -2,19 +2,29 @@ package fr.pantheonsorbonne.cri;
 
 import static org.junit.Assert.assertTrue;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.stream.StreamSource;
+
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() throws JAXBException {
+
+		JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		
+		
+		StreamSource ss = new StreamSource(App.class.getClassLoader().getResourceAsStream("choreography.xml"));
+		JAXBElement<TDefinitions> o =  jaxbUnmarshaller.unmarshal(ss,TDefinitions.class);
+
+		assertTrue("we don't unmarshall domain object",
+				o.getValue().getClass().getName().equals("fr.pantheonsorbonne.cri.TDefinitions"));
+
+	}
 }
