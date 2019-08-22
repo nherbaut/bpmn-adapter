f63a3520faa05b729bd30bb282b7303862d9951d
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-08-22 16:38:39 +0200

    almost valid bpmn2OAI

--- src/main/java/fr/pantheonsorbonne/cri/App.java
+++ src/main/java/fr/pantheonsorbonne/cri/App.java
@@ -1,23 +1,37 @@
 package fr.pantheonsorbonne.cri;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.namespace.QName;
+
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.Encoding;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 
 public class App {
 
@@ -29,54 +43,114 @@ public class App {
 		return res;
 	}
 
+	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	class MessageTypeDTO {
+		public MessageTypeDTO(TMessage message, Class<?> klass) {
+			this.message = message;
+			this.klass = klass;
+		}
+
+		public TMessage message;
+		public Class<?> klass;
+	}
+
 	public static void main(String[] args) throws JAXBException, JsonProcessingException {
 
-		BPMNFacade diag = new BPMNFacade("choreograph.xml");
+		File bpmnFile = new File("target/classes/choreography.xml");
+
+		BPMNFacade diag = new BPMNFacade(bpmnFile);
 
 		Content jsonContent = new Content().addMediaType("application/json",
 				new MediaType().addEncoding("UTF-8", new Encoding()));
 		Info info = new Info().contact(new Contact().email("nicolas.herbaut@univ-paris1.fr").name("Nicolas Herbaut")
 				.url("https://nextnet.top")).version("1.0.0");
 
-		
-		
-//		for (TParticipant participant : diag.getChoreography().getParticipant()) {
-//			OpenAPI oai = new OpenAPI().info(info.title(participant.getName()));
-//
-//			Map<TChoreographyTask, QName> taskMessageMap = diag.getChoreographyTasks().stream()
-//					.filter(t -> t.getInitiatingParticipantRef().getLocalPart().contains(participant.getId()))
-//					.collect(Collectors.toMap((TChoreographyTask t1) -> t1, (TChoreographyTask t2) -> t2.getMessageFlowRef().get(0)));
-//
-//			System.out.println(participant.getName() + "\t" + participant.getId());
-//			taskMessageMap.entrySet().stream().map(t -> "in " + t.getKey().getName() + " sends " + t.getValue().getLocalPart()).forEach(System.out::println);
-//			System.out.println("\n\n");
-//		}
-		
-		
-		diag.getChoreography().getMessageFlow().stream()
-		.map( m -> diag.getParticipant(m.getSourceRef()).getName()  + " sends " + 
-					diag.getMessage(m.getMessageRef()).getName() + " to " + 
-					diag.getParticipant(m.getTargetRef()).getName())
-		.forEach(System.out::println);
+		for (TParticipant participant : diag.getChoreography().getParticipant()) {
+			LOGGER.debug("for participant {}", participant.getName());
+			OpenAPI oai = new OpenAPI().info(info.title(participant.getName()));
+			List<TChoreographyTask> tasksWithIncommingMessages = diag.getChoreographyTasks().stream()
+					.filter(t -> !t.getInitiatingParticipantRef().getLocalPart().equals(participant.getName()))
+					.collect(Collectors.toList());
 
-//		for (TChoreographyTask task : diag.getChoreographyTasks()) {
-//			OpenAPI oai = new OpenAPI().info(info.title(task.getName()));
-//
-//			String participant1=task.getParticipantRef().get(0).getLocalPart();
-//			String messageFlow1=task.getMessageFlowRef().get(0).getLocalPart();
-//			
-//			String participant2=task.getParticipantRef().get(1).getLocalPart();
-//			String messageFlow2=task.getParticipantRef().get(1).getLocalPart();
-//			
-//			oai.path("/" + task.getName().replace(" ", "_"),
-//					new PathItem().get(new Operation().description("getty").responses(new ApiResponses()
-//							.addApiResponse("200", new ApiResponse().description("desc").content(jsonContent)
-//
-//							))));
-//
-//			System.out.println(getObjectMapper().writer(new DefaultPrettyPrinter()).writeValueAsString(oai));
-//		}
+			io.swagger.v3.oas.models.Paths paths = new io.swagger.v3.oas.models.Paths();
+			oai.paths(paths);
+			for (TChoreographyTask task : tasksWithIncommingMessages) {
+				LOGGER.debug("for incomming task {}", task.getName());
 
+				String endpointName = "/" + prettyEndpoint(task.getName());
+
+				List<Class<?>> klasses = task.getMessageFlowRef().stream()
+						.map(m -> diag.getMessage(diag.getMessageFlow(m).getMessageRef()))
+						.map(m -> diag.getTypeFromMessage(m)).collect(Collectors.toList());
+
+				Operation op = new Operation();
+				for (QName m : task.getMessageFlowRef()) {
+
+					PathItem item = new PathItem();
+					paths.addPathItem(endpointName, item);
+					item.description(task.getName());
+
+					if (klasses.size() == 1) {
+
+						ApiResponse response = new ApiResponse()
+								.$ref("#/components/schemas/" + klasses.get(0).getName());
+						op.responses(new ApiResponses().addApiResponse("200", response));
+						item.get(op);
+					} else {
+						RequestBody request = new RequestBody().required(true)
+								.content(new Content().addMediaType("application/json", new MediaType().schema(
+										new Schema().$ref("#/components/schemas/" + klasses.get(0).getName()))));
+						op.requestBody(request);
+						ApiResponse response = new ApiResponse()
+								.$ref("#/components/schemas/" + klasses.get(1).getName());
+						op.responses(new ApiResponses().addApiResponse("200", response));
+						item.post(op);
+					}
+
+					
+
+				}
+
+			}
+
+			oai.components(diag.getComponents());
+
+			try (FileWriter writer = new FileWriter(new File(participant.getName() + ".yaml"))) {
+				writer.write(toYaml(oai));
+				LOGGER.debug("file {} written", participant.getName() + ".yaml");
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+
+		}
+
+//		diag.getChoreography().getMessageFlow().stream()
+//				.map(m -> diag.getParticipant(m.getSourceRef()).getName() + " sends "
+//						+ diag.getMessage(m.getMessageRef()).getName() + " to "
+//						+ diag.getParticipant(m.getTargetRef()).getName())
+//				.forEach(System.out::println);
+
+	}
+
+	private static String toYaml(OpenAPI api) {
+		try {
+			String oaiString = getObjectMapper().writer(new DefaultPrettyPrinter()).writeValueAsString(api);
+			JsonNode jsonNodeTree = new ObjectMapper().readTree(oaiString);
+
+			return new YAMLMapper().writeValueAsString(jsonNodeTree);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	private static String capitalize(String str) {
+		return str.substring(0, 1).toUpperCase() + str.substring(1);
+	}
+
+	private static String prettyEndpoint(String s) {
+		return Arrays.stream(s.split(" ")).peek(App::capitalize).collect(Collectors.joining());
 	}
 
 	protected static ObjectMapper getObjectMapper() {
--- src/main/java/fr/pantheonsorbonne/cri/BPMNFacade.java
+++ src/main/java/fr/pantheonsorbonne/cri/BPMNFacade.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,29 +22,39 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.stream.StreamSource;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.ETypedElement;
+import org.eclipse.xsd.ecore.XSDEcoreBuilder;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 import com.google.common.base.Strings;
 import com.google.common.io.CharStreams;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.parser.util.OpenAPIDeserializer;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.description.type.TypeDescription.Generic;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 
-class BPMNFacade {
+public class BPMNFacade {
 
 	private static final String BPMN2_DEFINITIONS = "bpmn2:definitions";
 	private static final String XSD_ELEMENT = "xsd:element";
@@ -47,7 +62,21 @@ class BPMNFacade {
 	private static final String XSD_COMPLEX_TYPE = "xsd:complexType";
 	private final TDefinitions definitions;
 	private final Map<String, String> ns = new HashMap<String, String>();
-	private final Map<String, String> types = new HashMap<String, String>();
+	private final Map<String, Collection<Class<?>>> types = new HashMap<>();
+
+	class OpenAPIDeserializer2 extends OpenAPIDeserializer {
+		public Schema getSchema(ObjectNode node, String location) {
+			ParseResult pr = new ParseResult();
+
+			Schema res = getSchema(node, location, pr);
+			if (!pr.isValid()) {
+				throw new RuntimeException(pr.getMessages().stream().collect(Collectors.joining()));
+			}
+			return res;
+
+		}
+
+	}
 
 	class JsonTypeAndFormatHelper {
 		public String type;
@@ -86,6 +115,7 @@ class BPMNFacade {
 			Map.entry("xsd:integer", new JsonTypeAndFormatHelper("number", "integer"))
 
 	);
+	private File bpmnFile;
 
 	private InputStream getExampleChoreographModel() {
 
@@ -99,81 +129,40 @@ class BPMNFacade {
 		return new File(path).listFiles();
 	}
 
-	public Components getComonents(String resourcePath) {
+	public Components getComponents() {
+		Components components = new Components();
+		ObjectMapper mapper = new ObjectMapper();
+		JsonSchemaGenerator generator = new JsonSchemaGenerator(mapper);
+
 		try {
-			Components components = new Components();
 
-			try (InputStream is = App.class.getClassLoader().getResourceAsStream(resourcePath)) {
+			for (Map.Entry<String, Collection<Class<?>>> type : this.types.entrySet()) {
+				for (Class<?> klass : type.getValue()) {
 
-				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-				DocumentBuilder builder = factory.newDocumentBuilder();
-				Document document = builder.parse(is);
-				Element root = document.getDocumentElement();
-
-				Map<String, String> nsMap = new HashMap<String, String>();
-				String targetNamespace = "";
-				for (int i = 0; i < root.getAttributes().getLength(); i++) {
-					Node namespace = root.getAttributes().item(i);
-					if (namespace.getNodeName().startsWith("xmlns:")) {
-						nsMap.put(namespace.getNodeName().split(":")[1], namespace.getNodeValue());
-					} else if (namespace.getNodeName().equals("targetNamespace")) {
-						targetNamespace = namespace.getNodeValue();
-					}
+					//create a json schema from this class
+					JsonSchema jsonSchema = generator.generateSchema(klass);
+					String jsonSchemaStr = mapper.writer().writeValueAsString(jsonSchema);
+					
+					//parse the schema as json
+					ObjectNode objectNode = new ObjectMapper().readValue(jsonSchemaStr, ObjectNode.class);
+					
+					//use the unsealed Deserializer to generate the schema
+					Schema<?> schema = new OpenAPIDeserializer2().getSchema(objectNode, "");
+					components.addSchemas(klass.getName(), schema);
 
 				}
 
-				NodeList children = root.getChildNodes();
-				for (int i = 0; i < children.getLength(); i++) {
-					org.w3c.dom.Node node = children.item(i);
-
-					if (XSD_COMPLEX_TYPE.equals(node.getNodeName())) {
-						Schema<String> schema = new Schema<String>();
-						schema.setType("object");
-
-						String typeName = node.getAttributes().getNamedItem("name").getNodeValue();
-						components.addSchemas(typeName, schema);
-
-						System.out.println("handle " + typeName);
-						for (int j = 0; j < node.getChildNodes().getLength(); j++) {
-							Node sequence = node.getChildNodes().item(j);
-
-							if (XSD_SEQUENCE.equals(sequence.getNodeName())) {
-								System.out.println("\txsd:sequence");
-
-								for (int k = 0; k < sequence.getChildNodes().getLength(); k++) {
-									Node element = sequence.getChildNodes().item(k);
-									if (XSD_ELEMENT.equals(element.getNodeName())) {
-										String name = element.getAttributes().getNamedItem("name").getNodeValue();
-										String type = element.getAttributes().getNamedItem("type").getNodeValue();
-
-										JsonTypeAndFormatHelper jsonType = null;
-										jsonType = new JsonTypeAndFormatHelper(name, type);
-
-										schema.addProperties(name, jsonType.toSchema());
-										schema.addRequiredItem(name);
-
-										if (basicTypesMapping.containsKey(type)) {
-											System.out.println(
-													"\t\t" + name + " ( " + basicTypesMapping.get(type) + " ) ");
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-
-				System.out.println(App.getObjectMapper().writer(new DefaultPrettyPrinter()).writeValueAsString(components));
-				return components;
 			}
-		} catch (IOException | ParserConfigurationException | SAXException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
 
+		} catch (IOException e) {
+			System.out.println(e);
 		}
+		
+		return components;
+
 	}
 
-	private TDefinitions readBPMNFromEmbdedModel(String modelName) {
+	private TDefinitions readBPMNFromEmbdedModel(File bpmnFile) {
 		try {
 			JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
@@ -222,13 +211,14 @@ class BPMNFacade {
 		}
 	}
 
-	public BPMNFacade(String name) {
+	public BPMNFacade(File bpmnFile) {
 
-		writeToStandardOutputWithModuleJsonSchema(TChoreographyTask.class.getName());
-		this.definitions = readBPMNFromEmbdedModel(name);
+		this.bpmnFile = bpmnFile;
+		this.definitions = readBPMNFromEmbdedModel(bpmnFile);
 
 		this.definitions.getImport().stream().filter(i -> i.getLocation().equals(i.getLocation()))
-				.forEach(i -> types.put(i.getNamespace(), readResourceFromClassPath(i.getLocation())));
+				.forEach(i -> types.put(i.getNamespace(),
+						getExternalTypes(Paths.get(bpmnFile.getParentFile().getPath(), i.getLocation()).toFile())));
 
 	}
 
@@ -239,14 +229,22 @@ class BPMNFacade {
 	}
 
 	private <T extends TBaseElement> Stream<T> getDefinitionElements(Class<T> klass) {
-		return definitions.getRootElement().stream().map(e -> e.getValue()).filter(e -> klass.isInstance(e))
+		return definitions.getRootElement().stream()
 
-				.map(e -> klass.cast(e));
+				.map(e -> e.getValue())
+				// .peek(System.out::println)
+				.filter(e -> klass.isInstance(e)).map(e -> klass.cast(e));
 	}
 
 	public TMessage getMessage(QName qname) {
 		return this.getDefinitionElements(TMessage.class).filter(m -> qname.getLocalPart().equals("#" + m.getId()))
 				.findFirst().orElseThrow(NoSuchElementException::new);
+	}
+
+	public TMessageFlow getMessageFlow(QName qname) {
+		return this.getChoreography().getMessageFlow().stream()
+				.filter(m -> qname.getLocalPart().equals("#" + m.getId())).findFirst()
+				.orElseThrow(() -> new NoSuchElementException(qname.toString()));
 	}
 
 	public TParticipant getParticipant(QName qname) {
@@ -255,8 +253,111 @@ class BPMNFacade {
 				.orElseThrow(NoSuchElementException::new);
 	}
 
+	public TItemDefinition getItemDef(QName qname) {
+		return this.getDefinitionElements(TItemDefinition.class)
+				.filter(m -> qname.getLocalPart().equals("#" + m.getId())).findFirst()
+				.orElseThrow(NoSuchElementException::new);
+	}
+
 	private void writeToStandardOutputWithModuleJsonSchema(final String fullyQualifiedClassName) {
 
+	}
+
+	private static final String GENERATED_PACKAGE = "";
+
+	private Collection<Class<?>> getExternalTypes(File path) {
+
+		Collection<Class<?>> res = new ArrayList<Class<?>>();
+		URI schemaURI = URI.createFileURI(path.getPath());
+		XSDEcoreBuilder xsdEcoreBuilder = new XSDEcoreBuilder();
+
+		Collection<EObject> eCorePackages = xsdEcoreBuilder.generate(schemaURI);
+		Deque<EClassifier> xsdKlasses = new ArrayDeque<EClassifier>(eCorePackages.stream().findFirst().get().eContents()
+				.stream().map(o -> (EClassifier) o).collect(Collectors.toList()));
+
+		ByteBuddy bb = new ByteBuddy();
+		typeBuilder: while (!xsdKlasses.isEmpty()) {
+
+			System.out.println("### " + xsdKlasses.size() + " to GO!");
+
+			EClassifier klassifier = xsdKlasses.poll();
+			if (klassifier.getName().equals("DocumentRoot")) {
+				continue;
+			}
+			System.out.println("trying to generate dynamic type for " + klassifier.getName());
+			DynamicType.Builder<?> typeBuilder = null;
+
+			if (klassifier instanceof EClass) {
+				typeBuilder = bb.subclass(Object.class);
+				EClass klass = (EClass) klassifier;
+				for (int i = 0; i < klass.getFeatureCount(); i++) {
+					ETypedElement feature = klass.getEStructuralFeature(i);
+					if(feature.getName().equals("group")) {
+						continue;
+					}
+					Class<?> attributeClass = feature.getEType().getInstanceClass();
+
+					if (attributeClass == null) {
+						// can be a complex type, not sure if defined...
+						try {
+							attributeClass = Class.forName(GENERATED_PACKAGE + feature.getEType().getName());
+						} catch (ClassNotFoundException e) {
+							attributeClass = null;
+						}
+
+						if (attributeClass == null) {
+							// type not already defined in the classloader
+							System.out.println("missing type" + feature.getEType().getName()
+									+ ", deferring complex type creation");
+							xsdKlasses.addLast(klassifier);
+							continue typeBuilder;
+						}
+					}
+					String attributeName = feature.getName();
+					if (feature.getUpperBound() != 1) {
+						// List
+						Generic attributeList = TypeDescription.Generic.Builder
+								.parameterizedType(List.class, attributeClass).build();
+						typeBuilder = typeBuilder.defineField(attributeName, attributeList,
+								net.bytebuddy.description.modifier.Visibility.PUBLIC);
+					} else {
+						// scalar
+						typeBuilder = typeBuilder.defineField(attributeName, attributeClass,
+								net.bytebuddy.description.modifier.Visibility.PUBLIC);
+					}
+
+				}
+			} else if (klassifier instanceof EEnum) {
+				EEnum eenum = (EEnum) klassifier;
+				typeBuilder = bb.makeEnumeration(
+						eenum.getELiterals().stream().map(e -> e.getName()).collect(Collectors.toSet()));
+			} else {
+				System.out.println("don't know what to do with this" + klassifier + " [DROPING]");
+				continue typeBuilder;
+			}
+
+			typeBuilder = typeBuilder.name(GENERATED_PACKAGE + klassifier.getName());
+			Class<?> xsdType = typeBuilder.make()
+					.load(getClass().getClassLoader(), ClassLoadingStrategy.Default.INJECTION).getLoaded();
+			res.add(xsdType);
+
+		}
+
+		return res;
+	}
+
+	public Class<?> getTypeFromMessage(TMessage message) {
+
+		TItemDefinition itemDef = this.getItemDef(message.getItemRef());
+		String xsdName = itemDef.getStructureRef().getLocalPart();
+
+		Collection<Class<?>> klasses = this.types
+				.get(this.getItemDef(message.getItemRef()).getStructureRef().getNamespaceURI());
+		return klasses.stream()
+				// .peek(System.out::println)
+				// comparison is done ignore the case, since dynamic classes are capitalized
+				.filter(k -> k.getName().equalsIgnoreCase(GENERATED_PACKAGE + xsdName)).findAny()
+				.orElseThrow(() -> new NoSuchElementException(xsdName));
 	}
 
 }
